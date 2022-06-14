5610e1d42cfe07ba2d127df0d476326df691b6a9
kidow <wcgo2ling@gmail.com>
2022-06-14 21:33:48 +0900

    Backdrop 에 Portal 추가

--- src/components/Backdrop/index.tsx
+++ src/components/Backdrop/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import type { FC } from 'react'
-import { Spinner } from 'components'
+import { Spinner, Portal } from 'components'
 
 interface Props {
   loading: boolean
@@ -13,12 +13,12 @@ const Backdrop: FC<Props> = ({ loading }) => {
   }, [loading])
   if (!loading) return null
   return (
-    <>
+    <Portal role="progressbar">
       <div className="fixed inset-0 bg-black cursor-progress opacity-30" />
       <span className="fixed -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 cursor-progress">
         <Spinner className="w-10 h-10" />
       </span>
-    </>
+    </Portal>
   )
 }
 
