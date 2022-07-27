a0d0c081c084f31d4c01f71201a67fd370d0c95b
kidow <wcgo2ling@gmail.com>
2022-07-27 14:48:48 +0900

    Textarea 작업중

--- src/components/Textarea/index.tsx
+++ src/components/Textarea/index.tsx
@@ -1,10 +1,48 @@
-import type { FC } from 'react'
+import type {
+  FC,
+  DetailedHTMLProps,
+  KeyboardEvent,
+  TextareaHTMLAttributes,
+  ReactNode
+} from 'react'
+import classnames from 'classnames'
 
-export interface Props {}
+export interface Props
+  extends DetailedHTMLProps<
+    TextareaHTMLAttributes<HTMLTextAreaElement>,
+    HTMLTextAreaElement
+  > {
+  onEnter?: () => void
+  info?: ReactNode
+  error?: ReactNode
+  placeholder?: string
+  float?: boolean
+  fullWidth?: boolean
+}
 interface State {}
 
-const Textarea: FC<Props> = () => {
-  return <>Textarea</>
+const Textarea: FC<Props> = ({
+  onEnter,
+  info,
+  error,
+  placeholder,
+  fullWidth,
+  float,
+  ...props
+}) => {
+  const onKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey && !!onEnter) onEnter()
+  }
+  return (
+    <div>
+      <textarea
+        {...props}
+        onKeyDown={onKeyDown}
+        className={classnames('focus:outline-none')}
+      />
+      <label htmlFor=""></label>
+    </div>
+  )
 }
 
 export default Textarea
