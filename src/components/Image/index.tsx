5ca03c5f0142439d94dcd57254508e3ad449a14d
kidow <wcgo2ling@gmail.com>
2022-07-29 00:33:23 +0900

    Image

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -29,11 +29,13 @@ const Image: FC<Props> = ({ ...props }) => {
             aria-modal="true"
             role="dialog"
           >
-            <div className="flex min-h-screen items-end justify-center p-0 text-center md:block">
+            <div
+              className="flex min-h-screen items-end justify-center p-0 text-center md:block"
+              onClick={() => setState({ isOpen: false })}
+            >
               <div
                 className="fixed inset-0 bg-black opacity-30 transition-opacity"
                 aria-hidden="true"
-                onClick={() => setState({ isOpen: false })}
               ></div>
               <span
                 className="hidden h-screen align-middle md:inline-block"
@@ -43,14 +45,10 @@ const Image: FC<Props> = ({ ...props }) => {
               </span>
               <div
                 className={classnames(
-                  `my-8 inline-block w-full transform overflow-hidden align-middle shadow-xl transition-all sm:px-8`
+                  `my-8 inline-block w-full transform overflow-hidden align-middle transition-all sm:px-8`
                 )}
               >
-                <img
-                  {...props}
-                  className=""
-                  onClick={() => setState({ isOpen: false })}
-                />
+                <img {...props} className="mx-auto select-none shadow-xl" />
               </div>
             </div>
           </div>,
