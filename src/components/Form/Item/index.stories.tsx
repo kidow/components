ff06efece019574914532b2d33e80e7b1ed7e0c7
kidow <wcgo2ling@gmail.com>
2022-08-18 15:33:09 +0900

    Data Entry 점검

--- src/components/Form/Item/index.stories.tsx
+++ src/components/Form/Item/index.stories.tsx
@@ -1,9 +1,21 @@
-import FormItem from '.'
+import Component from '.'
 import type { ComponentMeta, ComponentStory } from '@storybook/react'
 
 export default {
-  title: 'Data Entry/FormItem',
-  component: FormItem
-} as ComponentMeta<typeof FormItem>
+  title: 'Data Entry/Form',
+  component: Component,
+  argTypes: {
+    label: {
+      control: { type: 'text' },
+      defaultValue: 'label'
+    },
+    children: {
+      control: { type: 'text' },
+      defaultValue: 'children'
+    }
+  }
+} as ComponentMeta<typeof Component>
 
-export const Primary: ComponentStory<typeof FormItem> = () => <FormItem />
+export const Item: ComponentStory<typeof Component> = ({ ...props }) => (
+  <Component {...props} />
+)
--- src/components/Upload/index.stories.tsx
+++ src/components/Upload/index.stories.tsx
@@ -2,7 +2,7 @@ import Component from '.'
 import type { ComponentMeta, ComponentStory } from '@storybook/react'
 
 export default {
-  title: 'Data Entry/Form',
+  title: 'Data Entry/Upload',
   component: Component
 } as ComponentMeta<typeof Component>
 
