240dda6f7b056318f48a2b06bfdccb830eceb9e9
kidow <wcgo2ling@gmail.com>
2022-08-19 16:47:59 +0900

    Rating 추가

--- src/components/Rating/index.stories.tsx
+++ src/components/Rating/index.stories.tsx
@@ -3,7 +3,32 @@ import type { ComponentMeta, ComponentStory } from '@storybook/react'
 
 export default {
   title: 'Data Entry',
-  component: Component
+  component: Component,
+  argTypes: {
+    value: {
+      control: { type: 'number', min: 0, max: 10 },
+      defaultValue: 0
+    },
+    onChange: {
+      action: 'onChange'
+    },
+    readOnly: {
+      control: { type: 'boolean' },
+      defaultValue: false
+    },
+    disabled: {
+      control: { type: 'boolean' },
+      defaultValue: false
+    },
+    half: {
+      control: { type: 'boolean' },
+      defaultValue: false
+    },
+    count: {
+      control: { type: 'number', min: 0, max: 10 },
+      defaultValue: 5
+    }
+  }
 } as ComponentMeta<typeof Component>
 
 export const Rating: ComponentStory<typeof Component> = ({ ...props }) => (
--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -1,33 +1,166 @@
+import { useMemo, useCallback } from 'react'
 import type { FC } from 'react'
+import classnames from 'classnames'
+import { useEffect } from 'react'
 
-export interface Props {}
+export interface Props {
+  value: number
+  onChange: (value: number) => void
+  readOnly?: boolean
+  disabled?: boolean
+  count?: number
+  half?: boolean
+}
 interface State {}
 
-const Rating: FC<Props> = () => {
+const Rating: FC<Props> = ({
+  value,
+  onChange,
+  readOnly,
+  disabled,
+  count = 5,
+  half
+}) => {
+  const onMouseLeave = useCallback(() => {
+    for (let i = 0; i < value; i++) {
+      const second = document.getElementById(`rating-second-${i}`)
+      if (second) second.style.color = ACTIVE_COLOR
+    }
+    for (let i = Math.floor(value); i < count; i++) {
+      const second = document.getElementById(`rating-second-${i}`)
+      if (second) second.style.color = INACTIVE_COLOR
+    }
+
+    if (half) {
+      const first = document.getElementById(`rating-first-${value}`)
+      const second = document.getElementById(
+        `rating-second-${Math.floor(value)}`
+      )
+      if (first) first.style.color = ACTIVE_COLOR
+      if (second) second.style.color = INACTIVE_COLOR
+
+      for (let i = 0; i < value; i++) {
+        const first = document.getElementById(`rating-first-${i}`)
+        if (first) first.style.color = ACTIVE_COLOR
+      }
+      for (let i = Math.ceil(value); i < count; i++) {
+        const first = document.getElementById(`rating-first-${i}`)
+        if (first) first.style.color = INACTIVE_COLOR
+      }
+    }
+  }, [value, half])
+
+  const onMouseEnter = useCallback((index: number) => {
+    for (let i = 0; i <= index; i++) {
+      const second = document.getElementById(`rating-second-${i}`)
+      if (second) second.style.color = ACTIVE_COLOR
+
+      if (half) {
+        for (let j = 0; j <= index; j++) {
+          const first = document.getElementById(`rating-first-${j}`)
+          if (first) first.style.color = ACTIVE_COLOR
+        }
+      }
+    }
+    for (let i = index + 1; i < count; i++) {
+      const second = document.getElementById(`rating-second-${i}`)
+      if (second) second.style.color = INACTIVE_COLOR
+
+      if (half) {
+        for (let j = index + 1; j < count; j++) {
+          const first = document.getElementById(`rating-first-${j}`)
+          if (first) first.style.color = INACTIVE_COLOR
+        }
+      }
+    }
+  }, [])
+
+  const onHalfMouseEnter = useCallback((index: number) => {
+    for (let i = 0; i < index; i++) {
+      const first = document.getElementById(`rating-first-${i}`)
+      const second = document.getElementById(`rating-second-${i}`)
+      if (first) first.style.color = ACTIVE_COLOR
+      if (second) second.style.color = ACTIVE_COLOR
+    }
+
+    const first = document.getElementById(`rating-first-${index}`)
+    if (first) first.style.color = ACTIVE_COLOR
+    const second = document.getElementById(`rating-second-${index}`)
+    if (second) second.style.color = INACTIVE_COLOR
+
+    for (let i = index + 1; i < count; i++) {
+      const first = document.getElementById(`rating-first-${i}`)
+      const second = document.getElementById(`rating-second-${i}`)
+      if (first) first.style.color = INACTIVE_COLOR
+      if (second) second.style.color = INACTIVE_COLOR
+    }
+  }, [])
+
+  const ACTIVE_COLOR: string = useMemo(() => '#fbbf24 ', [])
+  const INACTIVE_COLOR: string = useMemo(() => '#d1d5db', [])
+
+  useEffect(() => {
+    if (!value) return
+    for (let i = 0; i < value; i++) {
+      const second = document.getElementById(`rating-second-${i}`)
+      if (second) second.style.color = ACTIVE_COLOR
+    }
+    if (half) {
+      for (let i = 0; i < Math.ceil(value); i++) {
+        const first = document.getElementById(`rating-first-${i}`)
+        if (first) first.style.color = ACTIVE_COLOR
+      }
+    }
+  }, [])
   return (
-    <div className="flex items-center gap-1">
-      {Array.from({ length: 5 }).map((_, key) => (
-        <svg
-          version="1.1"
-          id="Layer_1"
-          x="0px"
-          y="0px"
-          viewBox="0 0 280.124 280.124"
-          className="h-10 w-10"
+    <ul
+      role="radiogroup"
+      className={classnames('flex select-none', {
+        'cursor-not-allowed opacity-70': disabled
+      })}
+    >
+      {Array.from({ length: count }).map((_, key) => (
+        <li
+          key={key}
+          aria-checked={value > key ? 'true' : 'false'}
+          aria-posinset={key + 1}
+          aria-setsize={count}
+          tabIndex={disabled ? -1 : 0}
+          role="radio"
+          className={classnames('text-xl text-gray-300', {
+            'cursor-pointer duration-150 hover:scale-125':
+              !disabled && !readOnly,
+            relative: half
+          })}
+          onMouseLeave={disabled || readOnly ? undefined : onMouseLeave}
         >
-          <path
-            fill="#000"
-            d="M280.124,106.914l-92.059-6.598L140.057,4.441l-48.55,95.874L0,106.914l61.282,74.015
-            l-17.519,94.754l96.294-43.614l96.294,43.606l-17.799-94.754C218.553,180.919,280.124,106.914,280.124,106.914z"
-          />
-          <polygon
-            fill="#f4b459"
-            points="236.352,275.683 218.553,180.92 280.071,106.975 280.071,106.905 188.065,100.315 
-            140.057,4.441 140.057,232.068 	"
-          />
-        </svg>
+          {half && (
+            <span
+              id={`rating-first-${key}`}
+              onMouseEnter={
+                disabled || readOnly ? undefined : () => onHalfMouseEnter(key)
+              }
+              onClick={
+                disabled || readOnly ? undefined : () => onChange(key + 0.5)
+              }
+              className="absolute left-0 top-0 h-full w-1/2 overflow-hidden duration-150"
+            >
+              ★
+            </span>
+          )}
+          <span
+            id={`rating-second-${key}`}
+            onMouseEnter={
+              disabled || readOnly ? undefined : () => onMouseEnter(key)
+            }
+            onClick={disabled || readOnly ? undefined : () => onChange(key + 1)}
+            className="duration-150"
+          >
+            ★
+          </span>
+        </li>
       ))}
-    </div>
+    </ul>
   )
 }
 
